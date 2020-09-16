3f6f81350ce7d0c2927761de2806b8544a18a621
Felix <felixmann00@gmail.com>
2020-09-16 10:35:59 +0200

    Initial commit

---
+++ src/ChatRework/Main/Chat.java
@@ -0,0 +1,13 @@
+package ChatRework.Main;
+
+/**
+ * @author Felix Mann
+ * @version 1.0
+ * @since 2020-September-16
+ */
+
+public class Chat {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
